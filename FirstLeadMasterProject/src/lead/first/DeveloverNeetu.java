33ffaeb4c24b6aea1836c3f6c050f05ce4973363
Neetu <Neetu@DESKTOP-0QSIVDF>
2022-12-25 00:30:59 +0530

    Hi neetu here I added DeveloverNeetu.class after ayush Singh

---
+++ FirstLeadMasterProject/src/lead/first/DeveloverNeetu.java
@@ -0,0 +1,11 @@
+package lead.first;
+
+public class DeveloverNeetu {
+
+	public static void main(String[] args) {
+		System.out.println("hi myself Neetu devi--neetu.03011984@gmail.com-neetu3184");
+		
+
+	}
+
+}
