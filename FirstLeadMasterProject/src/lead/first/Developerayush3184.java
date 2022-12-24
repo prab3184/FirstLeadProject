11af7092c2e022e0ddba351ea284ae20dbeb9481
ayush3184 <Prabhakar@DESKTOP-0QSIVDF>
2022-12-24 22:57:07 +0530

    second consecutive commit second developer (neetu3184) has not done any commit/push till now Hi gorget to modify sysout in class added by me in developerayush3184

--- FirstLeadMasterProject/src/lead/first/Developerayush3184.java
+++ FirstLeadMasterProject/src/lead/first/Developerayush3184.java
@@ -3,7 +3,7 @@ package lead.first;
 public class Developerayush3184 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hi i am pranee.3184@gmail.com--ayush3184");
 
 	}
 
