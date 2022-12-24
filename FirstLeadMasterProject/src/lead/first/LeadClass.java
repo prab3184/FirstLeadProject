5e63dc85874a675d959b51db001af55759e4d6ee
Prabhakar <Prabhakar@DESKTOP-0QSIVDF>
2022-12-24 22:01:52 +0530

    Leads structre of project

---
+++ FirstLeadMasterProject/src/lead/first/LeadClass.java
@@ -0,0 +1,11 @@
+package lead.first;
+
+public class LeadClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hi I am Lead-(prabhakariitr3184@gmail.com --prab3184) "
+				+ "creating first project for git Hub Repo for to collaborator");
+
+	}
+
+}
