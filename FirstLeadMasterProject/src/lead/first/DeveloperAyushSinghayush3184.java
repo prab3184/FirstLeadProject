510ae009d35da6aebc63dcb3c1cb4daa1ea93fa5
neetu <neetur@DESKTOP-0QSIVDF>
2022-12-26 15:34:26 +0530

    Second change after merge rule change

--- FirstLeadMasterProject/src/lead/first/DeveloperAyushSinghayush3184.java
+++ FirstLeadMasterProject/src/lead/first/DeveloperAyushSinghayush3184.java
@@ -4,7 +4,8 @@ public class DeveloperAyushSinghayush3184 {
 
 	public static void main(String[] args) {
 		System.out.println("Hi myself Ayush Singh-pranee.3184@gmail.com-ayush3184");
-		System.out.println("Hi myself Ayush Singh-pranee.3184@gmail.com-ayush3184--26/12/22");
+		System.out.println("Hi myself Ayush Singh-pranee.3184@gmail.com-ayush3184--26/12/22-1");
+		System.out.println("Hi myself Ayush Singh-pranee.3184@gmail.com-ayush3184--26/12/22-2");
 
 
 	}
