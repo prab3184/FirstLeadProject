ccb83c84161bc51f3d814561607972764cff6f79
Neetu <Neetu@DESKTOP-0QSIVDF>
2022-12-26 14:53:44 +0530

    new commit in line 7 in developed ayush singh/pranee.3184@gmail.com

--- FirstLeadMasterProject/src/lead/first/DeveloperAyushSinghayush3184.java
+++ FirstLeadMasterProject/src/lead/first/DeveloperAyushSinghayush3184.java
@@ -4,6 +4,7 @@ public class DeveloperAyushSinghayush3184 {
 
 	public static void main(String[] args) {
 		System.out.println("Hi myself Ayush Singh-pranee.3184@gmail.com-ayush3184");
+		System.out.println("Change in lin 7 by Neetu-neetu3184/neetu.03011984@gmail.com");
 
 	}
 
